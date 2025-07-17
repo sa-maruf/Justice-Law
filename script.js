1368cfdf6ed9b6ae63382116fbbf872d36d50b73
unknown <Sanowar-Alam-Maruf>
2025-07-17 14:28:26 +0600

    some commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let mobMenuBtn = document.getElementById('mob-menu-btn');
+let mobMenu = document.getElementById('mob-menu');
+
+mobMenuBtn.addEventListener('click' , ()=> {
+    mobMenu.classList.toggle('active')
+})
